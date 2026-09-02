af3834cd38b7511e0cc3a8eb7e238358cb8a962e
davieping58-rgb <davieping58@gmail.com>
2026-09-02 01:02:44 +0100

    Expose live XP command center bridge

--- desktop/preload.js
+++ desktop/preload.js
@@ -1,2 +1,10 @@
 const { contextBridge, ipcRenderer } = require('electron');
-contextBridge.exposeInMainWorld('xpDesktop',{toggleFullscreen:()=>ipcRenderer.invoke('xp:fullscreen')});
+contextBridge.exposeInMainWorld('xpDesktop',{
+  toggleFullscreen:()=>ipcRenderer.invoke('xp:fullscreen'),
+  telemetry:()=>ipcRenderer.invoke('xp:telemetry'),
+  coreHealth:()=>ipcRenderer.invoke('xp:core-health'),
+  chat:(message)=>ipcRenderer.invoke('xp:chat',message),
+  openUrl:(url)=>ipcRenderer.invoke('xp:open-url',url),
+  chooseImage:()=>ipcRenderer.invoke('xp:choose-image'),
+  openFile:(path)=>ipcRenderer.invoke('xp:open-file',path)
+});
