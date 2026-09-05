bd3b45b19a57c3b4b37f8ffb98b5427e39306e02
davieping58-rgb <davieping58@gmail.com>
2026-09-05 22:34:53 +0100

    Use branded Vercel proxy for Tattie APK

--- api/tattie/download.js
+++
@@ -1,6 +0,0 @@
-const APK='https://github.com/davieping58-rgb/xp-ai-interface/releases/download/tattietcg-latest/TattieTCG.apk';
-
-export default function handler(req,res){
-  res.setHeader('Cache-Control','no-store');
-  return res.redirect(302,APK);
-}
